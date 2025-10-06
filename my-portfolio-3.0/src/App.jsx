4dc32443d12a8efdf372840d23ab151b1aafc272
DinethSheveen <fdineth4000@gmail.com>
2025-10-06 21:47:23 +0100

    Imported Hero to teh application and created some animations

--- my-portfolio-3.0/src/App.jsx
+++ my-portfolio-3.0/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./App.css"
 import Navbar from './assets/Components/Navbar/Navbar'
+import Hero from "./assets/Components/Hero/Hero"
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Hero/>
     </>
   )
 }
