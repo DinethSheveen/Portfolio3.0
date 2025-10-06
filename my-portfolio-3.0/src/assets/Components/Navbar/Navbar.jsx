95d86627bee89f5d037192421200a60fe8d09774
DinethSheveen <fdineth4000@gmail.com>
2025-10-06 19:24:52 +0100

    Updated the menu links

--- my-portfolio-3.0/src/assets/Components/Navbar/Navbar.jsx
+++ my-portfolio-3.0/src/assets/Components/Navbar/Navbar.jsx
@@ -5,18 +5,17 @@ function Navbar() {
     <nav className='fixed top-0 w-full'>
         <div className='md:mx-8 md:my-4'>
             {/* FLEX CONTAINER */}
-            <div className="text-white flex justify-between items-center bg-gray-900 px-8 py-4 rounded-2xl">
+            <div className="text-white flex justify-between items-center bg-gray-900 px-8 py-4 md:rounded-2xl">
                 {/* LOGO */}
                 <p className='text-white font-bold cursor-pointer'><a href="">Dineth | DSF</a></p>
                 {/* MENY ITEMS */}
-                <div className='hidden md:flex gap-4'>
-                    <a href="" className='transition-all hover:font-bold hover:tracking-wider'>Work</a>
-                    <a href="" className='transition-all hover:font-bold hover:tracking-wider'>Skills</a>
-                    <a href="" className='transition-all hover:font-bold hover:tracking-wider'>Experience</a>
-                    <a href="" className='transition-all hover:font-bold hover:tracking-wider'>Projects</a>
+                <div className='hidden sm:flex gap-4'>
+                    <a href="#skills" className='transition-all hover:font-bold hover:tracking-wider'>Skills</a>
+                    <a href="#experience" className='transition-all hover:font-bold hover:tracking-wider'>Certificates</a>
+                    <a href="#projects" className='transition-all hover:font-bold hover:tracking-wider'>Projects</a>
                 </div>
                 {/* BUTTON */}
-                <button className='cursor-pointer transition-all bg-sky-200 text-black px-4 py-2 rounded-[10px] hover:text-white hover:bg-transparent hover:border-white border-1'>Contact me</button>
+                <button className='cursor-pointer transition-all bg-sky-200 text-black px-4 py-2 rounded-[10px] hover:text-white hover:bg-transparent hover:border-white border-1'><a href="#contact">Contact me</a></button>
             </div>
         </div>
     </nav>
