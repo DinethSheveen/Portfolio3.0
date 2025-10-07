96e2b8d4c16f8b91a45d9a48f69013c5ccaa27d5
DinethSheveen <fdineth4000@gmail.com>
2025-10-07 10:01:00 +0100

    Passed animation words from App to Hero as props

--- my-portfolio-3.0/src/App.jsx
+++ my-portfolio-3.0/src/App.jsx
@@ -2,12 +2,43 @@ import React from 'react'
 import "./App.css"
 import Navbar from './assets/Components/Navbar/Navbar'
 import Hero from "./assets/Components/Hero/Hero"
+import { LiaLaptopCodeSolid } from "react-icons/lia";
+import { RiAlignItemBottomFill } from "react-icons/ri";
+import { RiCompassDiscoverLine } from "react-icons/ri";
 
 function App() {
+
+  const heroWords = [
+        {
+            word : "ideas",
+            icon : <RiCompassDiscoverLine/>
+        },
+        {
+            word : "products",
+            icon : <RiAlignItemBottomFill/>
+        },
+        {
+            word : "code",
+            icon : <LiaLaptopCodeSolid/>
+        },
+                {
+            word : "concepts",
+            icon : <RiCompassDiscoverLine/>
+        },
+        {
+            word : "designs",
+            icon : <RiAlignItemBottomFill/>
+        },
+        {
+            word : "visions",
+            icon : <LiaLaptopCodeSolid/>
+        },
+    ]
+
   return (
     <>
     <Navbar/>
-    <Hero/>
+    <Hero heroWords={heroWords}/>
     </>
   )
 }
--- my-portfolio-3.0/src/assets/Components/Hero/Hero.jsx
+++ my-portfolio-3.0/src/assets/Components/Hero/Hero.jsx
@@ -1,38 +1,7 @@
 import React from 'react'
 import { FaRegArrowAltCircleDown } from "react-icons/fa";
-import { LiaLaptopCodeSolid } from "react-icons/lia";
-import { RiAlignItemBottomFill } from "react-icons/ri";
-import { RiCompassDiscoverLine } from "react-icons/ri";
 
-function Hero() {
-
-    const array = [
-        {
-            word : "ideas",
-            icon : <RiCompassDiscoverLine/>
-        },
-        {
-            word : "products",
-            icon : <RiAlignItemBottomFill/>
-        },
-        {
-            word : "code",
-            icon : <LiaLaptopCodeSolid/>
-        },
-                {
-            word : "concepts",
-            icon : <RiCompassDiscoverLine/>
-        },
-        {
-            word : "designs",
-            icon : <RiAlignItemBottomFill/>
-        },
-        {
-            word : "code",
-            icon : <LiaLaptopCodeSolid/>
-        },
-    ]
-
+function Hero({heroWords}) {
   return (
     <div className='text-white mt-32'>
         {/* FLEX-CONTAINER */}
@@ -43,10 +12,10 @@ function Hero() {
                     <h1 className='text-2xl md:text-5xl flex gap-2 items-center'>
                         Shaping 
                         <span className='overflow-hidden'>
-                            <span className='flex flex-col gap-12 justify-center items-center h-10 md:h-12 slider-animate'>{array.map((arr)=>{return(
-                                <span className="flex items-center gap-2">
-                                    {arr.icon}
-                                    {arr.word}
+                            <span className='flex flex-col gap-12 justify-center items-center h-10 md:h-12 slider-animate'>{heroWords.map((heroWord)=>{return(
+                                <span className="flex items-center gap-2 text-gray-400">
+                                    {heroWord.icon}
+                                    {heroWord.word}
                                 </span>
                             )})}
                             </span>
@@ -57,7 +26,7 @@ function Hero() {
                     <h1 className='text-2xl md:text-5xl'>into real Projects</h1>
                 </div>
                 <div>
-                    <h1 className='text-2xl md:text-5xl'>that Deliver Results</h1>
+                    <h1 className='text-2xl md:text-5xl'>that Inspire Change</h1>
                 </div>
 
                 {/* PARA INTRO */}
@@ -67,7 +36,7 @@ function Hero() {
 
                 {/* BUTTON */}
                 <div>
-                    <button className='bg-sky-50 px-6 py-3 text-black rounded-[10px] hover:bg-sky-100 active:bg-sky-200'><a href="#projects" className='flex items-center gap-2'>See me work <FaRegArrowAltCircleDown className='arrow-animate'/></a></button>
+                    <button className='bg-gray-100 px-6 py-3 text-black rounded-[10px] hover:bg-gray-300 active:bg-gray-500'><a href="#projects" className='flex items-center gap-2'>See me work <FaRegArrowAltCircleDown className='arrow-animate'/></a></button>
                 </div>
 
             </div>
