b5a920c761fd9b4813b19c801b538129690a6f61
DinethSheveen <fdineth4000@gmail.com>
2025-10-07 14:14:58 +0100

    Utilized react countUp

--- my-portfolio-3.0/src/assets/Components/Experience/Experience.jsx
+++ my-portfolio-3.0/src/assets/Components/Experience/Experience.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import CountUp from "react-countup"
 
 function Experience({expContents}) {
   return (
     <>
         {/* EXPERIENCE SHOWCASE CONTAINER */}
-        <div className="mt-10 px-10 text-white">
+        <div className="mt-20 px-10 text-white">
             {/* GRID-CONTAINER */}
             <div className="grid gap-10 grid-cols-[repeat(auto-fit,minmax(220px,1fr))]">
                 {expContents.map((expContent,index)=>{return (
                     <div className="bg-gray-900 rounded-2xl px-4 py-6 shadow-md shadow-gray-600" key={index}>
-                        <h1 className='text-3xl mb-2'>{`${expContent.number}+`}</h1>
+                        <h1 className='text-3xl mb-2'><CountUp suffix="+" end={expContent.number}/></h1>
                         <p className='text-gray-300'>{expContent.wording}</p>
                     </div>
                 )})}
