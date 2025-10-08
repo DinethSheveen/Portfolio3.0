73da1105ff8707336ee61e7953df477c7f52bdb5
DinethSheveen <fdineth4000@gmail.com>
2025-10-08 18:51:57 +0100

    Created the projects showcase section from scratch

--- my-portfolio-3.0/src/assets/Components/Projects/Projects.jsx
+++ my-portfolio-3.0/src/assets/Components/Projects/Projects.jsx
@@ -1,46 +1,15 @@
-import React, { useRef } from "react";
 import project2 from "/images/project2.png";
-import { ScrollTrigger } from "gsap/all";
-import gsap from "gsap"
-import {useGSAP} from "@gsap/react"
 
 function Projects({ side_projects }) {
-
-  // REGISTERING THE SCROLLTRIGGER PLUGIN
-  gsap.registerPlugin(ScrollTrigger);
-  const scrollRef = useRef(null)
-
-  // USING THE SCROLLTRIGGER IN USEGSAP
-  useGSAP(()=>{
-    const cards = gsap.utils.toArray(scrollRef.current.querySelectorAll("div"))
-
-    cards.forEach((card)=>{
-      gsap.fromTo(card,{
-        y:40,
-        opacity:0
-      },{
-        y:0,
-        opacity:1,
-        scrollTrigger : {
-          trigger : card,
-          start : "top bottom",
-          end : "top 30%",
-          scrub:true
-        }
-      }
-    )
-    })
-  },[])
-
   return (
     <>
-      <div className="project-showcase py-20" ref={scrollRef}>
+      <div className="project-showcase px-10">
         {/* FLEX-CONTAINER */}
-        <div className="flex flex-col gap-4 flex-1 max-h-screen px-10 lg:flex-row lg:justify-center lg:gap-10">
+        <div className="flex flex-col md:flex-row lg:flex-row gap-6">
           {/* LEFT SECTION - PROJECT 01*/}
-          <div className="flex flex-col gap-2 w-full lg:w-[60%]">
+          <div className="flex flex-col gap-6 py-20">
             {/* IMAGE-CONTAINER */}
-            <div className="flex flex-1 h-[2rem]">
+            <div className="flex flex-1">
               <img
                 src={project2}
                 alt="movie search web app"
@@ -49,12 +18,12 @@ function Projects({ side_projects }) {
             </div>
             {/* DESCRIPTION */}
             <div className="project-description">
-              <p className="text-white text-3xl py-7 lg:text-2xl lg:py-4">CineScope: A React.js and OMDb, TMDB API-powered app for fast, interactive, and mobile-friendly movie exploration.</p>
-              <p className="text-[20px] text-gray-300 py-7 lg:text-[15px] lg:py-0">An app built with React JS and API Intergration for a fast, user-friendly experience.</p>
+              <p className="text-white my-2 text-2xl">CineScope: A React.js and OMDb, TMDB API-powered app for fast, interactive, and mobile-friendly movie exploration.</p>
+              <p className="text-gray-500">An app built with React JS and API Intergration for a fast, user-friendly experience.</p>
             </div>
           </div>
           {/* RIGHT SECTION - PROJECT 02 & 03*/}
-          <div className="flex flex-col gap-6 lg:w-[30%] justify-between">
+          <div className="flex flex-col gap-6">
             {/* IMAGE-CONTAINER */}
             {side_projects.map((side_project,index) => {
               return (
@@ -65,10 +34,10 @@ function Projects({ side_projects }) {
                     className="rounded-2xl"
                   />
                   <div className="project-description">
-                    <p className="text-white text-3xl py-7 lg:text-2xl lg:py-4">
+                    <p className="text-white my-2">
                       {side_project.title}
                     </p>
-                    <p className="text-[20px] text-gray-300 py-7 lg:text-[15px] lg:py-0">
+                    <p className="text-gray-500">
                       {side_project.description}
                     </p>
                   </div>
