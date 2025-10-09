07d9378420ca688afdd56f2bd881ac6562adbd9d
DinethSheveen <fdineth4000@gmail.com>
2025-10-09 18:04:43 +0100

    Changed the imports

--- my-portfolio-3.0/src/App.jsx
+++ my-portfolio-3.0/src/App.jsx
@@ -1,90 +1,88 @@
 import React from 'react'
 import "./App.css"
-import Navbar from './assets/Components/Navbar/Navbar'
-import Hero from "./assets/Components/Hero/Hero"
-import Projects from './assets/Components/Projects/Projects';
+import Navbar from './assets/Sections/Navbar/Navbar'
+import Hero from "./assets/Sections/Hero/Hero"
+import Projects from './assets/Sections/Projects/Projects';
 
-import Experience from './assets/Components/Experience/Experience';
+import Experience from './assets/Sections/Experience/Experience';
 import { LiaLaptopCodeSolid } from "react-icons/lia";
 import { RiAlignItemBottomFill } from "react-icons/ri";
 import { RiCompassDiscoverLine } from "react-icons/ri";
 
-import TechAnimation from './assets/Components/TechAnimation/TechAnimation';
+import TechAnimation from './assets/Sections/TechAnimation/TechAnimation';
 import { FaReact } from "react-icons/fa";
 import { FaNodeJs } from "react-icons/fa";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { SiMysql } from "react-icons/si";
-import { FaHtml5 } from "react-icons/fa";
-import { FaCss3Alt } from "react-icons/fa";
 import { FaJs } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaGitAlt } from "react-icons/fa";
-import Featured from './assets/Components/Featured/Featured';
+import Featured from './assets/Sections/Featured/Featured';
 
 function App() {
 
   // ANIMATION WORDS AND ICONS FOR THE HERO SECTION
   const heroWords = [
-        {
-            word : "ideas",
-            icon : <RiCompassDiscoverLine/>
-        },
-        {
-            word : "products",
-            icon : <RiAlignItemBottomFill/>
-        },
-        {
-            word : "code",
-            icon : <LiaLaptopCodeSolid/>
-        },
-                {
-            word : "concepts",
-            icon : <RiCompassDiscoverLine/>
-        },
-        {
-            word : "designs",
-            icon : <RiAlignItemBottomFill/>
-        },
-        {
-            word : "visions",
-            icon : <LiaLaptopCodeSolid/>
-        },
-    ]
+    {
+        word : "ideas",
+        icon : <RiCompassDiscoverLine/>
+    },
+    {
+        word : "products",
+        icon : <RiAlignItemBottomFill/>
+    },
+    {
+        word : "code",
+        icon : <LiaLaptopCodeSolid/>
+    },
+            {
+        word : "concepts",
+        icon : <RiCompassDiscoverLine/>
+    },
+    {
+        word : "designs",
+        icon : <RiAlignItemBottomFill/>
+    },
+    {
+        word : "visions",
+        icon : <LiaLaptopCodeSolid/>
+    },
+  ]
 
-    //NUMBERS AND WORDS FOR THE EXPERIENCE SHOWCASE
-    const expContents = [
-      {
-        number : 2,
-        wording : "Years of Experience"
-      },
-      {
-        number : 8,
-        wording : "Developer Toolkits"
-      },
-      {
-        number : 20,
-        wording : "Completed Projects"
-      },
-      {
-        number : 1000,
-        wording : "Git Commits"
-      }
-    ]
+  //NUMBERS AND WORDS FOR THE EXPERIENCE SHOWCASE
+  const expContents = [
+    {
+      number : 2,
+      wording : "Years of Experience"
+    },
+    {
+      number : 8,
+      wording : "Developer Toolkits"
+    },
+    {
+      number : 20,
+      wording : "Completed Projects"
+    },
+    {
+      number : 1000,
+      wording : "Git Commits"
+    }
+  ]
 
-    // SIDE PROJECTS OF THE PROJECT SECTION
-    const side_projects = [
-      {
-        image : "/images/project2.png",
-        alt: "gsap-landing-page",
-        title : "A visually appealing page built with React JS, TailwindCSS, and GSAP for a fluid user experience.",
-        description : "A 3d Landing page powered with GSAP, React JS and TailwindCSS ", 
-      },
-      {
-        image : "/images/project3.png",
-        alt : "e-commerce web app",
-        title : "E-Commerce Web App built with the use of React JS, CSS",
-        description : "A seamless, user-friendly online-store ", 
-      }
+  // SIDE PROJECTS OF THE PROJECT SECTION
+  const side_projects = [
+    {
+      image : "/images/project2.png",
+      alt: "gsap-landing-page",
+      title : "A visually appealing page built with React JS, TailwindCSS, and GSAP for a fluid user experience.",
+      description : "A 3d Landing page powered with GSAP, React JS and TailwindCSS ", 
+    },
+    {
+      image : "/images/project3.png",
+      alt : "e-commerce web app",
+      title : "E-Commerce Web App built with the use of React JS, CSS",
+      description : "A seamless, user-friendly online-store ", 
+    }
   ]
 
   // TECH ICONS TO THE TECH MARQUEE
