ac7a9b5f06fc0676974abdd3004a4a6eb00cb75a
DinethSheveen <fdineth4000@gmail.com>
2025-10-10 09:08:49 +0100

    Reordered the nav links

--- my-portfolio-3.0/src/assets/Sections/Navbar.jsx
+++ my-portfolio-3.0/src/assets/Sections/Navbar.jsx
@@ -10,9 +10,9 @@ function Navbar() {
                 <p className='text-white font-bold cursor-pointer'><a href="">Dineth | DSF</a></p>
                 {/* MENY ITEMS */}
                 <div className='hidden md:flex gap-4'>
-                    <a href="#skills" className='transition-all hover:font-bold hover:tracking-wider'>Skills</a>
-                    <a href="#experience" className='transition-all hover:font-bold hover:tracking-wider'>Certificates</a>
                     <a href="#projects" className='transition-all hover:font-bold hover:tracking-wider'>Projects</a>
+                    <a href="#skills" className='transition-all hover:font-bold hover:tracking-wider'>Skills</a>
+                    <a href="#experience" className='transition-all hover:font-bold hover:tracking-wider'>Qualifications</a>
                 </div>
                 {/* BUTTON */}
                 <button className='cursor-pointer transition-all bg-gray-700 px-4 py-2 rounded-[10px] hover:bg-transparent hover:border-white hover:border-1'><a href="#contact">Contact me</a></button>
