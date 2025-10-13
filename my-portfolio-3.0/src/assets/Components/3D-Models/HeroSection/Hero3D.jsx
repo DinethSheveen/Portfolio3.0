1177bb2578cfb8b56f1ba8f4a9937d761a97a889
DinethSheveen <fdineth4000@gmail.com>
2025-10-13 23:30:28 +0100

    useMediaQuery of react-responsive to manage the responsiveness

--- my-portfolio-3.0/src/assets/Components/3D-Models/HeroSection/Hero3D.jsx
+++ my-portfolio-3.0/src/assets/Components/3D-Models/HeroSection/Hero3D.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import {Canvas} from "@react-three/fiber"
 import {OrbitControls} from "@react-three/drei"
 import { HeroModel } from './HeroModel'
+import {useMediaQuery} from "react-responsive"
 
 function Hero3D() {
+
+  const isMobile = useMediaQuery({query:"(max-width:768px)"})
+
   return (
     <Canvas camera={{position:[0,0,15], fov:45, near:0.1, far:1000}}>
-      <ambientLight color="blue" intensity={2}/>
+      <ambientLight color="blue" intensity={0.6}/>
       <directionalLight position={[5,5,5]} intensity={1}/>
-      <OrbitControls maxDistance={10} minDistance={5}/>
+      <OrbitControls maxDistance={10} minDistance={5} enableZoom={!isMobile}/>
 
-      <group position={[1,-2.5,0]}>
+      <group position={isMobile?[0,-2,0]:[0,-2.5,0]} rotation={[0,-0.8,0]} scale={isMobile?0.9:1}>
         <HeroModel/>
       </group>
       
