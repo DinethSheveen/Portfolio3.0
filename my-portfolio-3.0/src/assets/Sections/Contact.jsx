90484888f68ee4c29aacd2bde51b97d963907e5c
DinethSheveen <fdineth4000@gmail.com>
2025-10-14 11:24:51 +0100

    Contact section

---
+++ my-portfolio-3.0/src/assets/Sections/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TitleHeader from '../Components/TitleHeader/TitleHeader'
+
+function Contact() {
+  return (
+    <>
+        <TitleHeader title="💬 Questions? Ideas? I'm all Ears ✨"/>
+    </>
+  )
+}
+
+export default Contact
