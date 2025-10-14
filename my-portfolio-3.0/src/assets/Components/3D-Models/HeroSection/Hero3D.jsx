de3104dc954c331fa30f51143c20cb655f995adf
DinethSheveen <fdineth4000@gmail.com>
2025-10-14 11:26:20 +0100

    Fixed lightings

--- my-portfolio-3.0/src/assets/Components/3D-Models/HeroSection/Hero3D.jsx
+++ my-portfolio-3.0/src/assets/Components/3D-Models/HeroSection/Hero3D.jsx
@@ -12,15 +12,18 @@ function Hero3D() {
     <Canvas camera={{position:[0,0,15], fov:45, near:0.1, far:1000}}>
       {/* LIGHTINGS */}
       <spotLight position={[-2,1,-4]} color="#b603fc" penumbra={0.2} angle={3} intensity={30}/>
-      <spotLight position={[2,1,-4]} color="#fc039d" penumbra={2} angle={3} intensity={15}/>
-      <spotLight position={[-2,-3,4]} color="#aba7aa" intensity={5}/>
+      <spotLight position={[2,1,-4]} color="#fc039d" penumbra={1} angle={3} intensity={15}/>
       <spotLight position={[2,-3,4]} color="#aba7aa" intensity={5}/>
-
+      <spotLight position={[-3,-2,-2]} color="#aba7aa" intensity={5}/>
+      
       {/* ORBIT CONTROLS */}
       <OrbitControls maxDistance={10} minDistance={5} enableZoom={!isMobile} maxPolarAngle={Math.PI/2} minPolarAngle={Math.PI/4}/>
 
       {/* GROUP TO HAVE MORE CONTROL */}
-      <group position={isMobile?[0,-2,0]:[0,-2.5,0]} rotation={[0,-0.8,0]} scale={isMobile?0.9:1}>
+      <group 
+      position={isMobile?[0,-2,0]:[0,-2.5,0]} 
+      rotation={[0,-1.4,0]}
+      scale={isMobile?0.9:1}>
         {/* 3D MODEL */}
         <HeroModel/>
       </group>
