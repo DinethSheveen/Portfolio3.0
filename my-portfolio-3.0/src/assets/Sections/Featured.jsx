9b0ded818db908cac6c0b36f2d3a58ddd1a2d777
DinethSheveen <fdineth4000@gmail.com>
2025-10-14 21:25:18 +0100

    Responsive on very large screens

--- my-portfolio-3.0/src/assets/Sections/Featured.jsx
+++ my-portfolio-3.0/src/assets/Sections/Featured.jsx
@@ -5,7 +5,7 @@ function Featured({featured_details}) {
     <div className='my-10 mx-10'>
         {/* FLEX CONTAINER */}
         <div className="">
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 justify-center items-center'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center items-center'>
                 {featured_details.map((featured_detail,index)=>{
                     return(
                         <div key={index} className='flex flex-col gap-3 text-white bg-gray-800 py-2 px-4 rounded-[10px]'>
