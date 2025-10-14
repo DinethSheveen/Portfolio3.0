af44a4669b8b783e80e42592393cdf61daa220db
DinethSheveen <fdineth4000@gmail.com>
2025-10-14 10:22:47 +0100

    Added min/max polar angles

--- my-portfolio-3.0/src/assets/Components/3D-Models/HeroSection/Hero3D.jsx
+++ my-portfolio-3.0/src/assets/Components/3D-Models/HeroSection/Hero3D.jsx
@@ -12,7 +12,7 @@ function Hero3D() {
     <Canvas camera={{position:[0,0,15], fov:45, near:0.1, far:1000}}>
       <ambientLight color="blue" intensity={0.6}/>
       <directionalLight position={[5,5,5]} intensity={1}/>
-      <OrbitControls maxDistance={10} minDistance={5} enableZoom={!isMobile}/>
+      <OrbitControls maxDistance={10} minDistance={5} enableZoom={!isMobile} maxPolarAngle={Math.PI/2} minPolarAngle={Math.PI/4}/>
 
       <group position={isMobile?[0,-2,0]:[0,-2.5,0]} rotation={[0,-0.8,0]} scale={isMobile?0.9:1}>
         <HeroModel/>
