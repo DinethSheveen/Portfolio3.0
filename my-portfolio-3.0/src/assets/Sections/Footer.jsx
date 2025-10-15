b9c1b55bbe21f47f278d4e7c881eb8011ad35be7
DinethSheveen <fdineth4000@gmail.com>
2025-10-15 11:14:45 +0100

    Used react-responsive to adopt the footer to mobile devices

--- my-portfolio-3.0/src/assets/Sections/Footer.jsx
+++ my-portfolio-3.0/src/assets/Sections/Footer.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { FaLinkedin } from "react-icons/fa";
 import { FaSquareGithub } from "react-icons/fa6";
+import { useMediaQuery } from 'react-responsive';
 
 function Footer() {
+
+    const isMobile = useMediaQuery({query:"(max-width:448px)"})
+
   return (
     <>
         <div className="footer py-6 bg-gray-900">
             {/* FLEX-CONTAINER */}
-            <div className="flex justify-between items-center px-6 text-white md:flex-row lg:flex-row xl:flex-row">
+            <div className={`flex justify-between items-center px-6 text-white md:flex-row lg:flex-row xl:flex-row ${isMobile?`flex-col gap-4`:``}`}>
                 {/* YEAR AND NAME */}
                 <div>
                     &copy;2025 | DINETH
