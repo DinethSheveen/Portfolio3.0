1b3fe8c0178892d98d74828dd84cc0f4fbca37a1
DinethSheveen <fdineth4000@gmail.com>
2025-10-16 10:57:49 +0100

    Updated the component with a header

--- my-portfolio-3.0/src/assets/Components/TitleHeader/TitleHeader.jsx
+++ my-portfolio-3.0/src/assets/Components/TitleHeader/TitleHeader.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-function TitleHeader({title}) {
+function TitleHeader({title, header}) {
 
   return (
+    <>
     <div className="flex justify-center items-center">
         <p className="text-gray-300 mt-5 mb-3 py-2 px-6 bg-gray-700 rounded-[20px]">{title}</p>
     </div>
+    <div className="text-2xl text-transparent bg-clip-text bg-gradient-to-r from-gray-500 to-gray-200 flex justify-center md:text-3xl lg:text-4xl">{header}</div>
+    </>
   )
 }
 
